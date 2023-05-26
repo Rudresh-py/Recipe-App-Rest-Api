612411946a1ed8b0f8e50c7e3e60ed0a3d645ebb
Rudreshcg <rudresha.gangaswamaiah@softsuave.com>
2023-05-26 14:27:30 +0530

    updated files

--- app/app/settings.py
+++ app/app/settings.py
@@ -19,14 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-d)yboxht+7%eycwc42%eb!@42i(o22@^c!ycy-6-b-#-ftqz$b'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
-ALLOWED_HOSTS = [
-    '0.0.0.0', 'localhost'
-]
+ALLOWED_HOSTS = []
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 # Application definition
 
